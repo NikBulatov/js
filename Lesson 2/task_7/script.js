68705c7f2a9ae718d324e4904ebffc7f8d7c7db5
Nikita Bulatov <nikita.bulatov@ringcentral.com>
2022-02-06 20:46:32 +0300

    task 7 unresolved

---
+++ Lesson 2/task_7/script.js
@@ -0,0 +1,6 @@
+// 7. *Сравнить null и 0. Попробуйте объяснить результат.
+console.log(null > 0); // false
+console.log(null === 0); // false
+console.log(null >= 0); // true С точки зрения математики, если у нас есть два числа, x и y, и если x не меньше, чем y,
+                            // тогда x должно быть больше чем y или равно ему.
+                            // https://habr.com/ru/company/ruvds/blog/337732/
