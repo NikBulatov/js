dddb6cd4c0eea91c5b1184b71bbd7ba235532a62
Nikita Bulatov <nikita.bulatov@ringcentral.com>
2022-02-17 20:42:26 +0300

    task_1

--- task_1/script.js
+++ task_1/script.js
@@ -4,4 +4,25 @@
 * Если число превышает 999, необходимо выдать соответствующее сообщение с помощью console.log и вернуть пустой объект.
 */
 
-const digits
+function digitsNumber(number) {
+    if (number <= 999 && number >= 0) {
+        let unit = number % 10;
+        let ten = Math.floor(number % 100 / 10);
+        let hundred = Math.floor(number / 100);
+
+        const digits = {
+            'units': unit,
+            'tens': ten,
+            'hundreds': hundred,
+        };
+
+        return digits;
+    }
+    else {
+        console.log('Input a number from 0 to 999!');
+        return {}
+    }
+};
+
+let num = 0;
+console.log(digitsNumber(num));
