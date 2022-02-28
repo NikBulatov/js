e7c1ab6f29872fd2fafdf0abb2c2a3dd29b171cb
Nikita Bulatov <nikita.bulatov@ringcentral.com>
2022-02-28 13:34:35 +0300

    task 1

--- task_1/script.js
+++ task_1/script.js
@@ -1,80 +1,54 @@
 "use strict";
+const notebook = {
+    id: 321,
+    name: 'NoteBook',
+    price: 45690,
+    quantity: 1,
+};
+const mouse = {
+    id: 369,
+    name: 'Mouse',
+    price: 1500,
+    quantity: 1,
+}
+const keyboard = {
+    id: 987,
+    name: 'Keyboard',
+    price: 2500,
+    quantity: 1,
+}
 
-/**
- *  Объект каталога товаров
- */
 const catalog = {
     catalogBlock: null,
     cart: null,
-    list: [
-        {
-            id_product: 123,
-            product_name: 'Ноутбук',
-            price: 45600,
-        },
-        {
-            id_product: 456,
-            product_name: 'Мышка',
-            price: 1000,
-        },
-        {
-            id_product: 245,
-            product_name: 'Клавиатура',
-            price: 1500,
-        }
-    ],
+    list: [notebook, mouse, keyboard],
 
-    /**
-     * Инициальзация каталога.
-     * @param catalogBlockClass - класс блока каталога
-     * @param cart - корзина
-     */
-    init(catalogBlockClass, cart) {
-        this.catalogBlock = document.querySelector(`.${catalogBlockClass}`);
+    init(catalogClassBlock, cart) {
+        this.catalogBlock = document.querySelector(`.${catalogClassBlock}`);
         this.cart = cart;
         this.render();
         this.addEventHandlers();
     },
 
-    /**
-     * Рендер каталога
-     */
     render() {
-        if (this.getCatalogListLength() > 0) {
+        if (this.list.length > 0) {
             this.renderCatalogList();
         } else {
             this.renderEmptyCatalog();
         }
     },
 
-    /**
-     * Добавляем обработку событий
-     */
     addEventHandlers() {
         this.catalogBlock.addEventListener('click', event => this.addToBasket(event));
     },
 
-    /**
-     * Метод добавления в корзину
-     */
     addToBasket(event) {
         if (!event.target.classList.contains('product__add-to-cart')) return;
         const id_product = +event.target.dataset.id_product;
-        const productToAdd = this.list.find((product) => product.id_product === id_product);
+        const productToAdd = this.list.find((product) => product.id === id_product);
         this.cart.addToBasket(productToAdd);
     },
 
-    /**
-     * Метод получения количества товаров в каталоге
-     * @returns {number}
-     */
-    getCatalogListLength() {
-        return this.list.length;
-    },
-
-    /**
-     * Рендер списка товаров
-     */
     renderCatalogList() {
         this.catalogBlock.innerHTML = '';
         this.list.forEach(item => {
@@ -82,119 +56,70 @@ const catalog = {
         });
     },
 
-    /**
-     * Рендер отдельного товара из списка
-     * @param item - товар
-     * @returns {string} - сгенерированая строка разметки
-     */
     renderCatalogItem(item) {
         return `<div class="product">
-                <h3>${item.product_name}</h3>
-                <p>${item.price} руб.</p>
-                <button class="product__add-to-cart" data-id_product="${item.id_product}">В корзину</button>
+                <h3>${item.name}</h3>
+                <p>${item.price} RUB</p>
+                <button class="product__add-to-cart" data-id_product="${item.id}">Add to cart</button>
             </div>`;
     },
 
-    /**
-     * Рендер пустого каталога
-     */
     renderEmptyCatalog() {
         this.catalogBlock.innerHTML = '';
-        this.catalogBlock.textContent = 'Каталог товаров пуст.';
+        this.catalogBlock.textContent = 'Catalog is empty';
     },
 };
 
-/**
- *  Объект корзины
- */
+
 const cart = {
     cartBlock: null,
     clrCartButton: null,
-    goods: [
-        {
-            id_product: 123,
-            product_name: 'Ноутбук',
-            price: 45600,
-            quantity: 2,
-        },
-    ],
+    goods: [notebook],
 
-    /**
-     * Метод инициальзации корзины
-     * @param cartBlockClass - класс блока корзины
-     * @param clrCartButton - класс кнопки очистки корзины
-     */
     init(cartBlockClass, clrCartButton) {
         this.cartBlock = document.querySelector(`.${cartBlockClass}`);
         this.clrCartButton = document.querySelector(`.${clrCartButton}`);
-
 
         this.addEventHandlers();
         this.render();
     },
 
-    /**
-     * Метод установки обработчиков событий
-     */
     addEventHandlers() {
-        this.clrCartButton.addEventListener('click', this.dropCart.bind(this));
+        this.clrCartButton.addEventListener('click', this.clearCart.bind(this));
     },
 
-    /**
-     * Метод очистки корзины
-     */
-    dropCart() {
+    clearCart() {
         this.goods = [];
         this.render();
     },
 
-    /**
-     * Рендер корзины
-     */
     render() {
-        if (this.getCartGoodsLength() > 0) {
+        if (this.goods.length > 0) {
             this.renderCartList();
         } else {
             this.renderEmptyCart();
         }
     },
 
-    /**
-     * Добавить товар
-     */
     addToBasket(product) {
         if (product) {
-            const findInBasket = this.goods.find((item) => product.id_product === item.id_product);
-            if (findInBasket) {
-                findInBasket.quantity++;
+            const isInBasket = this.goods.find((item) => product.id === item.id);
+            if (isInBasket) {
+                isInBasket.quantity++;
             } else {
                 this.goods.push({...product, quantity: 1});
             }
             this.render();
         } else {
-            alert('Ошибка добавления!');
+            alert('Value Error!');
         }
     },
 
-    /**
-     * Получение количества товаров в корзине
-     * @returns {number}
-     */
-    getCartGoodsLength() {
-        return this.goods.length;
-    },
-
-    /**
-     * Рендер пустой корзины
-     */
     renderEmptyCart() {
         this.cartBlock.innerHTML = '';
-        this.cartBlock.textContent = 'Корзина пуста.';
+        this.cartBlock.textContent = 'Cart is empty';
     },
 
-    /**
-     * Рендер списка товаров в корзине
-     */
     renderCartList() {
         this.cartBlock.innerHTML = '';
         this.goods.forEach(item => {
@@ -202,22 +127,14 @@ const cart = {
         });
     },
 
-    /**
-     * Рендер отдельного товара в корзине
-     * @param item - товар
-     * @returns {string} - сгененрированая строка разметки
-     */
     renderCartItem(item) {
         return `<div>
-                <h3>${item.product_name}</h3>
-                <p>${item.price} руб.</p>
-                <p>${item.quantity} шт.</p>
+                <h3>${item.name}</h3>
+                <p>${item.price} RUB</p>
+                <p>${item.quantity} items</p>
             </div>`;
     },
 };
 
-/**
- * Подключение каталога и корзины
- */
 catalog.init('catalog', cart);
 cart.init('cart', 'clr-cart');
